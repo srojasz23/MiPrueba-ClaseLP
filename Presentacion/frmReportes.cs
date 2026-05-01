5efc4d532d4842dea3646f5abcd6bfd2e72f0b9d
Stefa <Instructor@08SJLZ1P1103I01>
2026-04-30 19:47:46 -0500

    act 30.04

--- Presentacion/frmReportes.cs
+++ Presentacion/frmReportes.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using Entidad;
+using iText.Kernel.Pdf;
+using Negocio;
+using Negocio.BLL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,127 +13,225 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using Negocio;
-using Negocio.BLL;
-using Entidad;
-
-
-using ClosedXML.Excel; // esto es para excel    
-using iText.Kernel.Pdf; // esto es para pdf
-using iText.Layout; // esto es para pdf
-using iText.Layout.Element; // esto es para pdf
-using iText.Layout.Properties; // esto es para pdf
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 using iText.Kernel.Font;
 using iText.IO.Font.Constants;
-using DocumentFormat.OpenXml.Spreadsheet; // esto es para pdf
+
 
 namespace Presentacion
-{
-    public partial class frmReportes : Form
-    {
-        private readonly ReporteBLL _bll = new ReporteBLL();
-        private string _rol;
-        private string _usuario;
-        public frmReportes(string usuario, string rol)
-        {
-            InitializeComponent();
 
-            _rol = rol;
+{
+
+    public partial class frmReportes : Form
+
+    {
+
+        private readonly ReporteBLL _bll = new ReporteBLL();
+
+        private string _usuario;
+
+        private string _rol;
+
+        public frmReportes(string usuario, string rol)
+
+        {
+
+            InitializeComponent();
             _usuario = usuario;
 
+            _rol = rol;
             this.Load += frmReportes_Load;
+
         }
 
+
+
+
+
         private void frmReportes_Load(object sender, EventArgs e)
+
         {
-            cboTipo.Items.Clear();
+
             cboTipo.Items.Add("Por Área");
             cboTipo.Items.Add("Contratos Activos");
             cboTipo.Items.Add("Contratos Vencidos");
             cboTipo.Items.Add("Alertas");
-
             cboTipo.SelectedIndex = 0;
 
-            if (_rol != "Operador")
+
+
+            if (_rol == "Operador")
 
                 btnExcel.Enabled = false;
 
         }
 
+
+
         private void btnGenerar_Click(object sender, EventArgs e)
+
         {
+
             switch (cboTipo.SelectedIndex)
+
             {
+
                 case 0:
+
                     dgv.DataSource = _bll.PorArea();
-                    _bll.RegistrarLog("Generó reporte por área", _usuario);
+
+                    _bll.RegistrarLog("Por Área", _usuario);
+
                     break;
+
+
+
                 case 1:
+
                     dgv.DataSource = _bll.Activos();
-                    _bll.RegistrarLog("Generó reporte de contratos activos", _usuario);
+                    _bll.RegistrarLog("Activos", _usuario);
+
                     break;
+
+
+
                 case 2:
+
                     dgv.DataSource = _bll.Vencidos();
-                    _bll.RegistrarLog("Generó reporte de contratos vencidos", _usuario);
+
+                    _bll.RegistrarLog("Vencidos", _usuario);
+
                     break;
+
+
+
                 case 3:
+
                     dgv.DataSource = _bll.Alertas();
-                    _bll.RegistrarLog("Generó reporte de alertas", _usuario);
+
+                    _bll.RegistrarLog("Alertas", _usuario);
+
                     break;
+
             }
+
         }
+
+
 
         private void btnCSV_Click(object sender, EventArgs e)
+
         {
+
             using var sfd = new SaveFileDialog();
-            sfd.Filter = "CSV files (*.csv)|*.csv";
+
+            sfd.Filter = "CSV (*.csv)|*.csv";
+
+
+
             if (sfd.ShowDialog() == DialogResult.OK)
+
             {
+
                 using var sw = new StreamWriter(sfd.FileName);
+
+
+
                 foreach (DataGridViewColumn col in dgv.Columns)
-                    sw.Write(col.HeaderText + ",");
+
+                    sw.Write(col.HeaderText + ";");
+
+
+
                 sw.WriteLine();
+
+
+
                 foreach (DataGridViewRow row in dgv.Rows)
+
                 {
+
+                    if (row.IsNewRow) continue;
+
+
+
+
+
                     foreach (DataGridViewCell cell in row.Cells)
 
-                        sw.Write(cell.Value + ",");
+                        sw.Write(cell.Value + ";");
+
+
 
                     sw.WriteLine();
+
                 }
 
-                MessageBox.Show("CSV Generado correctamente ");
+
+
+                MessageBox.Show("CSV generado");
+
             }
+
         }
 
+
+
         private void btnExcel_Click(object sender, EventArgs e)
+
         {
+
             if (dgv.Rows.Count == 0)
+
             {
-                MessageBox.Show("No hay datos para exportar.");
+
+                MessageBox.Show("No hay datos para exportar");
+
                 return;
+
             }
+
 
 
             using var sfd = new SaveFileDialog();
-            sfd.Filter = "Excel files (*.xlsx)|*.xlsx";
+
+            sfd.Filter = "Excel (*.xlsx)|*.xlsx";
+
             sfd.FileName = $"Reporte_{DateTime.Now:yyyyMMdd_HHmm}.xlsx";
 
+
+
             if (sfd.ShowDialog() == DialogResult.OK)
+
             {
+
                 var wb = new XLWorkbook();
+
                 var ws = wb.Worksheets.Add("Reporte");
 
-                // aca empieza el encabezado
+
+
+                // Encabezados
 
                 for (int i = 0; i < dgv.Columns.Count; i++)
+
                     ws.Cell(1, i + 1).Value = dgv.Columns[i].HeaderText;
 
-                // dats
+
+
+                // Datos
+
                 for (int i = 0; i < dgv.Rows.Count; i++)
 
                 {
+
                     if (dgv.Rows[i].IsNewRow) continue;
+
+
+
                     for (int j = 0; j < dgv.Columns.Count; j++)
 
                         ws.Cell(i + 2, j + 1).Value =
@@ -137,113 +240,186 @@ namespace Presentacion
 
                 }
 
+
+
                 wb.SaveAs(sfd.FileName);
+
+
+
                 MessageBox.Show("Excel generado correctamente");
 
-                wb.SaveAs(sfd.FileName);
-                MessageBox.Show("Excel generado correctamente.");
-
             }
+
         }
 
+
+
         private void btnPDF_Click(object sender, EventArgs e)
+
         {
+
             if (dgv.Rows.Count == 0)
+
             {
-                MessageBox.Show("No hay datos para exportar.");
+
+                MessageBox.Show("No hay datos para exportar");
+
                 return;
+
             }
 
-            // aca vamos a definir el nombre de la ubicacion de descarga del archivo
+
+
             using var sfd = new SaveFileDialog();
-            sfd.Filter = "PDF(*.pdf)|*.pdf";
-            sfd.FileName = $"ReportePDF_{DateTime.Now:yyyyMMdd_HHmm}.pdf";
 
-            if (sfd.ShowDialog() == DialogResult.OK) return;
+            sfd.Filter = "PDF (*.pdf)|*.pdf";
 
-            // crear el PDF 
+            sfd.FileName = $"Reporte_{DateTime.Now:yyyyMMdd_HHmm}.pdf";
+
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
 
             var writer = new PdfWriter(sfd.FileName);
+
             var pdf = new PdfDocument(writer);
+
             var document = new Document(pdf);
-            // fuentes 
+
+
+
+
+
             var fontNormal = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
             var fontBold = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
 
-            // formato del titulo
-            document.Add(new Paragraph("REPORTE DE EMPLEADOS DE LAS EMPRESA PALOMINO SAC")
-                .SetFont(fontBold)
-                .SetFontSize(17)
-                .SetTextAlignment(TextAlignment.CENTER));
 
-            // datos generales del reporte
+
+
+
+            document.Add(new Paragraph("REPORTE DE EMPLEADOS")
+
+              .SetFont(fontBold)
+
+              .SetFontSize(16)
+
+              .SetTextAlignment(TextAlignment.CENTER));
+
+
+
+
 
             document.Add(new Paragraph($"Usuario: {_usuario}")
-                .SetFont(fontNormal));
 
-            document.Add(new Paragraph($"Fecha: {DateTime.Now:dd/MM/yyyy}")
-                .SetFont(fontNormal));
+              .SetFont(fontNormal));
 
 
-            document.Add(new Paragraph(" ")); // espacio 
+
+            document.Add(new Paragraph($"Fecha: {DateTime.Now}")
+
+              .SetFont(fontNormal));
 
 
-            // aca insertamos la tabla
-             var table = new Table(dgv.Columns.Count);
 
-            // aca voy establecer el encabezado de la tabla
+            document.Add(new Paragraph(" ")); // espacio
 
-            foreach(DataGridViewColumn col in dgv.Columns)
+
+
+
+
+            var table = new Table(dgv.Columns.Count);
+
+
+
+            // Encabezados
+
+            foreach (DataGridViewColumn col in dgv.Columns)
+
             {
+
                 table.AddHeaderCell(
-                    new Cell().Add(
-                        new Paragraph(col.HeaderText)
-                        .SetFont(fontBold)));
+
+                  new Cell().Add(
+
+                    new Paragraph(col.HeaderText)
+
+                    .SetFont(fontBold)
+
+                  )
+
+                );
+
             }
+
+
 
             int totalRegistros = 0;
 
-            // aca traera las filas
+
+
+            // Filas
+
             foreach (DataGridViewRow row in dgv.Rows)
+
             {
+
                 if (row.IsNewRow) continue;
+
+
+
                 foreach (DataGridViewCell cell in row.Cells)
-                { 
+
+                {
+
                     table.AddCell(
-                        new Cell().Add(
+
+                      new Cell().Add(
+
                         new Paragraph(cell.Value?.ToString() ?? "")
-                        .SetFont(fontNormal)));
+
+                        .SetFont(fontNormal)
+
+                      )
+
+                    );
 
                 }
+
+
+
                 totalRegistros++;
-            
+
             }
+
+
 
             document.Add(table);
 
 
-            // total 
-            document.Add(new Paragraph(""));
-            document.Add(new Paragraph($"TOTAL DE REGISTROS: {totalRegistros}"))
-                .SetFont(fontBold); 
+
+
+
+            document.Add(new Paragraph(" "));
+
+            document.Add(new Paragraph($"TOTAL REGISTROS: {totalRegistros}")
+
+              .SetFont(fontBold));
+
+
+
+
 
             document.Close();
 
-            MessageBox.Show("PDF generado correctamente.");
 
 
-
-
-
-
-
-
-
-
-
-
+            MessageBox.Show("PDF generado correctamente");
 
         }
+
     }
+
+
 
 }
